1a55012f8d15e5b948adf4d38e9f5c112462dacd
Kamaal Farah <kamaal.f1@gmail.com>
2024-09-14 15:47:03 +0000

    Navigating after a while

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "export",
-  assetPrefix: process.env.ASSET_PREFIX,
+  assetPrefix: process.env.ASSET_PREFIX || ".",
 };
 
 export default nextConfig;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,29 @@
+"use client";
+
+import Button from "@/components/button";
 import Link from "@/components/link";
 
+import styles from "./page.module.css";
+
+const LINK = "https://apps.apple.com/bj/developer/kamaal-farah/id1502158009/";
+const NAVIGATION_TIMEOUT = 2000;
+
 function Home() {
+  function handleNavigateAfterAWhileClick() {
+    setTimeout(() => {
+      window.location.href = LINK;
+    }, NAVIGATION_TIMEOUT);
+  }
+
   return (
-    <div>
+    <div className={styles.page}>
       <h1>Links</h1>
-      <Link
-        link="https://apps.apple.com/bj/developer/kamaal-farah/id1502158009/"
-        aria-label="Apple store"
-      >
+      <Link link={LINK} aria-label="Apple store">
         <span>kamaal111</span>
       </Link>
+      <Button onClick={handleNavigateAfterAWhileClick}>
+        <span>Navigate after a while</span>
+      </Button>
     </div>
   );
 }
